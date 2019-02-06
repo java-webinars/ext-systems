bcbc0444b232e425fe5cf3c56f2de92d7b4a0a85
java-webinars <chestrick@yandex.ru>
2019-02-05 21:34:11 -0500

    Lesson_0088

---
+++ city-register/src/test/java/edu/javacourse/net/SimpleSocket.java
@@ -0,0 +1,30 @@
+package edu.javacourse.net;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class SimpleSocket
+{
+    @Test
+    public void simpleSocket() throws IOException {
+        Socket socket = new Socket("java-course.ru", 80);
+
+        InputStream is = socket.getInputStream();
+        OutputStream os = socket.getOutputStream();
+
+        String command = "GET /sitemap.xml HTTP/1.1\r\nHost:java-course.ru\r\n\r\n";
+        os.write(command.getBytes());
+        os.flush();
+
+        int c = 0;
+        while ( (c = is.read()) != -1) {
+            System.out.print((char)c);
+        }
+
+        socket.close();
+    }
+}
