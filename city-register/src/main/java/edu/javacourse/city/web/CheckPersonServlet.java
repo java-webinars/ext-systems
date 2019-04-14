0df1da634b7c220cea748367de6408119bdac795
java-webinars <chestrick@yandex.ru>
2019-04-14 18:56:09 -0400

    Lesson_0102

--- city-register/src/main/java/edu/javacourse/city/web/CheckPersonServlet.java
+++ city-register/src/main/java/edu/javacourse/city/web/CheckPersonServlet.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @WebServlet(name = "CheckPersonServlet", urlPatterns = {"/checkPerson"})
 public class CheckPersonServlet extends HttpServlet
@@ -34,16 +35,16 @@ public class CheckPersonServlet extends HttpServlet
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         req.setCharacterEncoding("UTF-8");
 
-        String surname = req.getParameter("surname");
         PersonRequest pr = new PersonRequest();
-        pr.setSurName(surname);
-        pr.setGivenName("Павел");
-        pr.setPatronymic("Николаевич");
-        pr.setDateOfBirth(LocalDate.of(1995, 3, 18));
-        pr.setStreetCode(1);
-        pr.setBuilding("10");
-        pr.setExtension("2");
-        pr.setApartment("121");
+        pr.setSurName(req.getParameter("surname"));
+        pr.setGivenName(req.getParameter("givenname"));
+        pr.setPatronymic(req.getParameter("partonymic"));
+        LocalDate dateOfBirth = LocalDate.parse(req.getParameter("dateOfBirth"), DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        pr.setDateOfBirth(dateOfBirth);
+        pr.setStreetCode(Integer.parseInt(req.getParameter("streetCode")));
+        pr.setBuilding(req.getParameter("building"));
+        pr.setExtension(req.getParameter("extension"));
+        pr.setApartment(req.getParameter("apartment"));
 
         try {
             PersonResponse ps = dao.checkPerson(pr);
