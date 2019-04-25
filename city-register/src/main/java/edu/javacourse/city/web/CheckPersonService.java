3e097ce0401039efbdd96eac79833c9e2bef4abf
java-webinars <chestrick@yandex.ru>
2019-04-24 21:18:06 -0400

    Lesson_0104

--- city-register/src/main/java/edu/javacourse/city/web/CheckPersonService.java
+++ city-register/src/main/java/edu/javacourse/city/web/CheckPersonService.java
@@ -1,13 +1,22 @@
 package edu.javacourse.city.web;
 
+import edu.javacourse.city.domain.PersonResponse;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
 
 @Path("/check")
 public class CheckPersonService
 {
     @GET
-    public String checkPerson() {
-        return "Simple String";
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public PersonResponse checkPerson(@PathParam("id") int simpleId,
+                                      @QueryParam("name") String simpleName) {
+        return new PersonResponse();
     }
 }
