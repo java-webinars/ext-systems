e8184953743fd7361952c1f39be96f2c86a9e63f
java-webinars <chestrick@yandex.ru>
2019-04-27 13:22:58 -0400

    Lesson_0106

--- city-register/src/main/java/edu/javacourse/city/web/CheckPersonService.java
+++ city-register/src/main/java/edu/javacourse/city/web/CheckPersonService.java
@@ -1,8 +1,16 @@
 package edu.javacourse.city.web;
 
+import edu.javacourse.city.dao.PersonCheckDao;
+import edu.javacourse.city.dao.PoolConnectionBuilder;
 import edu.javacourse.city.domain.PersonRequest;
 import edu.javacourse.city.domain.PersonResponse;
+import edu.javacourse.city.exception.PersonCheckException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.inject.Singleton;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
@@ -10,13 +18,25 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("/check")
+@Singleton
 public class CheckPersonService
 {
+    private static final Logger logger = LoggerFactory.getLogger(CheckPersonService.class);
+
+    private PersonCheckDao dao;
+
+    @PostConstruct
+    public void init() {
+        logger.info("SERVICE is created");
+        dao = new PersonCheckDao();
+        dao.setConnectionBuilder(new PoolConnectionBuilder());
+    }
+
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public PersonResponse checkPerson(PersonRequest request) {
-        System.out.println(request.toString());
-        return new PersonResponse();
+    public PersonResponse checkPerson(PersonRequest request) throws PersonCheckException {
+        logger.info(request.toString());
+        return dao.checkPerson(request);
     }
 }
