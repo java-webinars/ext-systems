bb92662712e2db5db7d87c0d3ef5a631f096db86
java-webinars <chestrick@yandex.ru>
2020-09-27 14:26:18 -0400

    Lesson_0148

--- student/src/main/java/edu/javacourse/student/rest/StudentController.java
+++ student/src/main/java/edu/javacourse/student/rest/StudentController.java
@@ -13,7 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RestController
@@ -38,5 +41,16 @@ public class StudentController
     public String checkParams(@PathVariable("checkId") Long checkId,
                               @RequestParam("comment") String comment) {
         return checkId + ":" + comment;
+    }
+
+    @PostMapping(path = "/photo", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public String uploadPhoto(@RequestParam("comment") String comment,
+                              @RequestParam("photoFile")MultipartFile photoFile) {
+        try (InputStream is = photoFile.getInputStream()) {
+            return  "Comment:" + comment + ", Name:" + photoFile.getName()
+                    + " File Name:" + photoFile.getOriginalFilename() + ", Size:" + is.available();
+        } catch(IOException ex) {
+            throw new RuntimeException(ex);
+        }
     }
 }
